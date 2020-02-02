634dde446a2bfc95ef2e9751d9e9fb978741a5d3
Ishita Sequeira <sequeira.i@northeastern.edu>
2020-02-02 16:02:32 -0500

    created endpoints

---
+++ project/src/main/java/com/bigdataindexing/project/BigDataIndexingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bigdataindexing.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataIndexingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataIndexingProjectApplication.class, args);
+	}
+
+}
